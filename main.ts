a8d2b23f30369076ac2086eee04b456dafa8d5de
Vítek Dolanský <vitek.dolansky.024@pslib.cz>
2025-05-17 17:12:54 +0200

    trim jen o 5

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 radio.setGroup(23)
 
+let trim = 0
+
 basic.forever(function () {
     let x = input.acceleration(Dimension.X); //left,right
     let y = input.acceleration(Dimension.Y); //forward,backwards
@@ -13,3 +15,17 @@ input.onButtonPressed(Button.AB, function() {
 input.onLogoEvent(TouchButtonEvent.Pressed, function() {
     radio.sendValue("drive", 1)
 })
+
+input.onButtonPressed(Button.A, function(){
+    trim -= 5
+    radio.sendValue("trim", trim)
+    basic.showNumber(trim)
+})
+
+input.onButtonPressed(Button.B, function () {
+    trim += 5
+    radio.sendValue("trim", trim)
+    basic.showNumber(trim)
+})
+
+
