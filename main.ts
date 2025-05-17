04762caeec4658d1ef98e3213fc7a81ddb87f2d9
Vítek Dolanský <vitek.dolansky.024@pslib.cz>
2025-05-17 16:22:09 +0200

    cely setupnuty a AB zastavuje auto, logo rozjizdi

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
+radio.setGroup(23)
 
+basic.forever(function () {
+    let x = input.acceleration(Dimension.X); //left,right
+    let y = input.acceleration(Dimension.Y); //forward,backwards
+    radio.sendValue("x", x); radio.sendValue("y", y); basic.pause(50);
+})
+
+input.onButtonPressed(Button.AB, function() {
+    radio.sendValue("drive", 0)
+})
+
+input.onLogoEvent(TouchButtonEvent.Pressed, function() {
+    radio.sendValue("drive", 1)
+})
