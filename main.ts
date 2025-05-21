3920b2bee2d9328f3bc842d4079b3e98bd1bb211
Vítek Dolanský <vitek.dolansky.024@pslib.cz>
2025-05-21 11:53:46 +0200

    ted to posila najednou

--- main.ts
+++ main.ts
@@ -6,7 +6,8 @@ let canDrive = true
 basic.forever(function () {
     let x = input.acceleration(Dimension.X); //left,right
     let y = input.acceleration(Dimension.Y); //forward,backwards
-    radio.sendValue("x", x); radio.sendValue("y", y); basic.pause(50);
+    radio.sendString(`${x},${y}`)
+    basic.pause(50);
 })
 
 input.onButtonPressed(Button.AB, function () {
